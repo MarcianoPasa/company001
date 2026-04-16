cbba68b7ccc0981801697be5c34de269031f6bf6
marcianopasa2 <marcianoap@gmail.com>
2026-04-16 19:30:48 -0300

    company001 - CRUD products - adjustments 0026

--- src/main/java/com/system/company001/Company001Application.java
+++ src/main/java/com/system/company001/Company001Application.java
@@ -2,8 +2,10 @@ package com.system.company001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class Company001Application {
 
 	static void main(String[] args) {
